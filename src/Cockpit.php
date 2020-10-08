d96307fc6a92c98baae73be483ab7b8b58cc9a8c
Boris DEHOUMON <borisdehoumon@gmail.com>
2020-10-08 15:16:09 +0100

    open checklist

---
+++ src/Cockpit.php
@@ -0,0 +1,7 @@
+<?php
+namespace Dcolsay\Cockpit;
+
+class Cockpit
+{
+
+}
---
+++ src/CockpitServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Dcolsay\Cockpit;
+
+use Dcolsay\Cockpit\Components\SideBar;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+class CockpitServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->bootResources();
+        $this->bootBladeComponents();
+    }
+
+    private function bootResources(): void
+    {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'cockpit');
+
+    }
+
+    private function bootBladeComponents(): void
+    {
+        Blade::component('side-bar', SideBar::class);
+    }
+}
